745f611b15c8ee7889901b5c9384be10b93e5cb1
Patrick Ware <101075639+Kronos1000@users.noreply.github.com>
2023-07-12 13:51:32 +1200

    update show question command
    Update command to return information that can be seen by end users.Previously this command was for dev use only

--- commands/showQuestions.js
+++ commands/showQuestions.js
@@ -2,23 +2,33 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { Question, writeQuestionsToFile } = require('../midori-quiz');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('showquestions')
-		.setDescription('show all questions currently in quiz bank '),
-		
-		
+  data: new SlashCommandBuilder()
+    .setName('showquestions')
+    .setDescription('show all questions currently in quiz bank '),
 
-	async execute(interaction) {
-		const quiz = require('../questions/quiz.json');
-		//console.log(quiz)
-		var keyCount  = Object.keys(quiz).length;
-		console.log("There are currently " +keyCount +  " questions in the quiz bank.");
-		const info = JSON.stringify((quiz));
-		const words = "words";
+  async execute(interaction) {
+    const quiz = require('../questions/quiz.json');
+    const keyCount = Object.keys(quiz).length;
+    console.log("There are currently " + keyCount + " questions in the quiz bank.");
+
+    let currentMessage = 'The Quiz bank contains the following questions:\n';
+    let index = 1;
+    for (const question of Object.values(quiz)) {
+      const questionText = question.question;
+      const newLine = `${index}. ${questionText}\n`;
+      const newMessage = currentMessage + newLine;
+
+      if (newMessage.length <= 2000) {
+        currentMessage = newMessage;
+        index++;
+      } else {
+        await interaction.reply(currentMessage);
+        currentMessage = newLine;
+        index = 2;
+      }
+    }
+
+    await interaction.reply(currentMessage);
 console.log(quiz);
-	await interaction.reply("Check System Log for details");
-
-
-	
-	},
+  },
 };
